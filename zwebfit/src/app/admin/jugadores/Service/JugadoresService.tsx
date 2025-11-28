db77aa05f68ef8ca2f1666b952b6f35a006cacb9
Mariela Higuera <0322103734@ut-tijuana.edu.mx>
2025-11-27 18:34:36 -0800

    Panel de Admin, Usuarios por ID

--- zwebfit/src/app/admin/jugadores/Service/JugadoresService.tsx
+++ zwebfit/src/app/admin/jugadores/Service/JugadoresService.tsx
@@ -3,27 +3,52 @@
 import { enviroment } from "@/lib/enviroment/enviroment";
 
 export class JugadoresService {
-
-  async getAll() {
-    const token = localStorage.getItem("admin_token");
-
-    const res = await fetch(`${enviroment.Production.ApiUrl}/usuarios/`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-
-    const json = await res.json();
-    const users = json.data;
-
-    return users.map((u: any) => ({
-      id: u.UserId,
-      name: u.Nombre,
-      email: u.Correo,
-      rolId: u.RolId,
-      estado: u.Estado,
-      fecha: u.FechaRegistro,
-    }));
-  }
+update(id: any, data: any) {
+  return fetch(`http://54.234.36.199/usuarios/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("admin_token")}`,
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
 }
+
+
+async getAll() {
+  const token = localStorage.getItem("admin_token");
+
+  if (!token) {
+    console.warn("❌ No existe admin_token en localStorage");
+    return []; // evita el crash
+  }
+
+  const res = await fetch(`${enviroment.Production.ApiUrl}/usuarios/`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const json = await res.json();
+
+  if (!json.data) {
+    console.warn("❌ La API devolvió un formato inesperado:", json);
+    return [];
+  }
+
+  const users = json.data;
+
+  return users.map((u: any) => ({
+    id: u.UserId,
+    name: u.Nombre,
+    email: u.Correo,
+    rolId: u.RolId,
+    estado: u.Estado,
+    fecha: u.FechaRegistro,
+  }));
+}
+
+
+}
+
